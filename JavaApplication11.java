749d3532f5760337e52e4fafd8c4ec8b0be89b2c
Elmezo <36506058+Elmezo@users.noreply.github.com>
2020-03-15 21:27:17 +0200

    Update JavaApplication11.java

--- JavaApplication11.java
+++ JavaApplication11.java
@@ -1,10 +1,21 @@
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class JavaApplication11 {
 
     public static void main(String[] args) {
-        int[] puzzle = {3, 1, 2, 5, 4, 8, 6, 7, 0};
+        // int[] puzzle = {1, 2, 4, 3, 0, 5, 7, 6, 8};
+        //*******************---------------------------------------------------------------
+        System.out.println("please enter the arrangement of the number in the puzzle");
+        Scanner s = new Scanner(System.in);
+        int puzzle[] = new int[9];
+        for (int i = 0; i < 9; i++) {
+            puzzle[i] = s.nextInt();
+        }
+       //*******************---------------------------------------------------------------
 
+        
+        
         Node root = new Node(puzzle);
         Bfs ui = new Bfs();
         LinkedList<Node> solution = ui.BreadthFisrstSearch(root);
